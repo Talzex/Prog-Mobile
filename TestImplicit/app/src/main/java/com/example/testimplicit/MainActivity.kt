6626e11154aa54b87d0c9fdf74129e8697a4be8b
tduthil <thomas.duthil@etu.u-bordeaux.fr>
2022-02-10 15:09:40 +0100

    Exo 10 fini

--- TestImplicit/app/src/main/java/com/example/testimplicit/MainActivity.kt
+++ TestImplicit/app/src/main/java/com/example/testimplicit/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity  : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,31 +26,49 @@ class MainActivity  : AppCompatActivity(),View.OnClickListener {
 
     override fun onClick(v: View?) {
         if (v != null) {
-            val phone = "+33618041926"
+            val phonenb = findViewById<EditText>(R.id.editTextPhone).text.toString()
+            val url = findViewById<EditText>(R.id.editTextURL).text.toString()
+            val lat = findViewById<EditText>(R.id.editTextLatitude).text.toString().toInt()
+            val long = findViewById<EditText>(R.id.editTextLongitude).text.toString().toInt()
             when(v.id) {
                 R.id.button -> {
-                    val intent = Intent(Intent.ACTION_SENDTO,Uri.fromParts("smsto",phone,null));
-                    intent.putExtra("sms_body", "The SMS text")
-                    startActivity(intent)
-                }
+                    if (phonenb != null){
+                        val intent = Intent(Intent.ACTION_SENDTO,Uri.fromParts("smsto",phonenb,null));
+                        intent.putExtra("sms_body", "The SMS text")
+                        startActivity(intent)
+                    }
 
-                    R.id.button2 -> {
-                        val mmsintent = Intent(Intent.ACTION_SENDTO,Uri.fromParts("mmsto", phone, null));
+                }
+                R.id.button2 -> {
+                    if (phonenb != null){
+                        val mmsintent = Intent(Intent.ACTION_SENDTO,Uri.fromParts("mmsto", phonenb, null));
                         mmsintent.putExtra("sms_body", "The MMS text")
                         startActivity(mmsintent)
                     }
-                    R.id.button3 -> {
-                        val callintent = Intent(Intent.ACTION_DIAL,Uri.fromParts("tel", phone, null));
+
+                }
+                R.id.button3 -> {
+                    if (phonenb != null){
+                        val callintent = Intent(Intent.ACTION_DIAL,Uri.fromParts("tel", phonenb, null));
                         startActivity(callintent)
                     }
-                    R.id.button4 -> {
-                        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+
+                }
+                R.id.button4 -> {
+                    if (url != null){
+                        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
                         startActivity(browserIntent)
                     }
-                    R.id.button5 -> {
-                        val mapintent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+CA+94043"))
+
+                }
+                R.id.button5 -> {
+                    if (lat != null && long != null){
+                        val mapintent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:$lat,$long"))
+                        mapintent.setPackage("com.google.android.apps.maps")
                         startActivity(mapintent)
                     }
+
+                }
                 }
             }
         }
